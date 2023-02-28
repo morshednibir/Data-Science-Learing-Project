8653d5382ca733dce63b4a6368a41c9dd994dbde
morshednibir <morshednibir@gmai.com>
2023-02-28 21:03:30 +0600

    updated

---
+++ Data_type R.R
@@ -0,0 +1,9 @@
+#logical
+
+a = TRUE
+print(class(a))
+is.vector(a)
+
+b = 96.336
+print(class(b))
+
---
+++ hello_world.py
@@ -0,0 +1,11 @@
+first = float(input("First Number: "))
+second = float(input("Second Number: "))
+sum = first+second
+substract = first-second
+multiply = first*second
+divide= first/second
+# this is addition
+print("The Result is: ", sum)
+print("The Result is: ",substract) # this is substraction
+print("The Result is: ",multiply)
+print("The Result is: ",divide
